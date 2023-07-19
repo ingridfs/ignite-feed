1b7966b4cc0852b37a9694ae6925417ab4a65b75
Ingrid Fernandes <ingrid.fernandes.santos@hotmail.com>
2023-07-19 12:12:03 -0300

    feat: add Header and Post component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import "./global.css";
+import styles from "./App.module.css";
 
 function App() {
-
   return (
-    <>
-      <h2>Hello world</h2>
-    </>
-  )
+    <div>
+      <Header></Header>
+
+      <div className={styles.wrapper}>
+        <aside> SIDE BAR </aside>
+
+        <main>
+          <Post
+            author="Ingrid"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+          <Post
+            author="Maria"
+            content="Vivamus fermentum vitae ex eu consequat. Aenean tempus ex non augue feugiat, non vestibulum magna fermentum."
+          />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
